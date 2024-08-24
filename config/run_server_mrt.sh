b947179c77f9a30bb6d34a9debe33f6cee637816
Tim F <120514393+tim-aero@users.noreply.github.com>
2024-08-24 19:44:20 +1000

    Updates to get MRT working

--- config/run_server_mrt.sh
+++ config/run_server_mrt.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --rm -tid -d -v `pwd`:/etc/aerospike/ -e "FEATURE_KEY_FILE=/etc/aerospike/features.conf" -e "LOGFILE=/var/log/aerospike/aerospike.log" --name aerospike_mrt -p 3000-3002:3000-3002 aerospike.jfrog.io/docker/aerospike/aerospike-server-enterprise-rc:latest
+docker run --rm -tid -d -v `pwd`:/etc/aerospike/ -e "FEATURE_KEY_FILE=/etc/aerospike/features.conf" -e "LOGFILE=/var/log/aerospike/aerospike.log" --name aerospike_mrt -p 3000-3002:3000-3002 aerospike-server-enterprise-rc:latest
+
+#docker run -d --name aerospike_mrt -p 3000:3000 -v $(pwd):/opt/aerospike/etc aerospike.jfrog.io/docker/aerospike/aerospike-server-enterprise-rc:latest
--- src/main/java/com/aerospike/demos/seat_reservation_demo/AerospikeController.java
+++ src/main/java/com/aerospike/demos/seat_reservation_demo/AerospikeController.java
@@ -1,13 +1,14 @@
 package com.aerospike.demos.seat_reservation_demo;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Bin;
 import com.aerospike.client.IAerospikeClient;
+import com.aerospike.client.Tran;
+import com.aerospike.client.policy.WritePolicy;
 
 @Controller
 @RestController
@@ -15,8 +16,14 @@ public class AerospikeController {
     @GetMapping("/demo/api/getDatabases") 
     public String getDatabases() {
         IAerospikeClient client = new AerospikeClient("localhost", 3000);
-        client.getTranVerifyPolicyDefault();
+        
+        Tran tran = new Tran();
+        WritePolicy wp = new WritePolicy();
+        wp.tran = tran;
+        client.put(wp, new Key("test", "testSet", 123), new Bin("name", "value"));
+        client.put(wp, new Key("test", "testSet", 456), new Bin("age", 37));
+        client.commit(tran);
+        client.close();
         return "Success";
     }
-
 }
