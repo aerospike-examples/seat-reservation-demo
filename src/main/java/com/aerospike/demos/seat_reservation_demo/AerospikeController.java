caf26e9904bc9c17d1b4be6a86359dfc87cfa5fa
Evan Cummack <ecummack@aerospike.com>
2024-08-24 01:32:19 +0000

    MRT boilerplate code in place. Server is crashing (see stacktrace in #dev-general), but Java code probably correct

--- src/main/java/com/aerospike/demos/seat_reservation_demo/AerospikeController.java
+++ src/main/java/com/aerospike/demos/seat_reservation_demo/AerospikeController.java
@@ -7,32 +7,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aerospike.client.AerospikeClient;
-import com.aerospike.client.AerospikeException;
 import com.aerospike.client.Bin;
 import com.aerospike.client.Key;
+import com.aerospike.client.Record;
+import com.aerospike.client.Txn;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
 
 @Controller
 @RestController
 public class AerospikeController {
-    @GetMapping("/demo/api/getDatabases")
-    public String getDatabases() {
+    @GetMapping("/tmp/testTxn")
+    public String testTxn() {
         AerospikeClient client = new AerospikeClient("localhost", 3000);
         if (client.isConnected()) {
+
+            Txn txn = new Txn();
+            System.out.println("Begin txn: " + txn.getId());
             try {
-                Key key = new Key("test", "demo", "demokey");
-                Bin bin = new Bin("greeting", "Hello, Aerospike!");
-                client.put(null, key, bin);
-                System.out.println("Record inserted successfully");
-            } catch (AerospikeException e) {
-                System.err.println("Error inserting record: " + e.getMessage());
+                WritePolicy wp = client.copyWritePolicyDefault();
+                wp.txn = txn;
+                Key key1 = new Key("test", "demo", 1);
+                client.put(wp, key1, new Bin("a", "val1"));
+                Key key2 = new Key("test", "demo", 2);
+                client.put(wp, key2, new Bin("b", "val2"));
+                Policy p = client.copyReadPolicyDefault();
+                p.txn = txn;
+                Key key3 = new Key("test", "demo", 3);
+                Record rec = client.get(p, key3);
+                WritePolicy dp = client.copyWritePolicyDefault();
+                dp.txn = txn;
+                dp.durableDelete = true; // Required when running delete in a MRT.
+                client.delete(dp, key3);
+                client.commit(txn);
+                client.close();
+            } catch (Throwable t) {
+                // Abort and rollback MRT (multi-record transaction) if any errors occur.
+                client.abort(txn);
+                client.close();
+                throw t;
             }
-            System.out.println("connected!");
-            client.close();
-            return "Success";
+            return "I did a transaction: " + String.valueOf(txn.getId());
+
         } else {
-            System.out.println("not connected!");
             client.close();
-            return "Failure";
+            return "Failed to connect to Aerospike database!";
         }
 
     }
