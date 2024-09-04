347d3701e4e2bc5237d7d9f5214f847e4ae6da1e
roimenashe <roimenashe5@gmail.com>
2024-09-04 11:40:00 +0300

    Separate Server and Simulator as modules (with different jars)

---
+++ simulator/src/main/java/dev/aerospike/ticketfaster/model/ShoppingCart.java
@@ -0,0 +1,25 @@
+package dev.aerospike.ticketfaster.model;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class ShoppingCart {
+    
+    public enum Status {
+        PENDING,
+        PURCHASED,
+        ABANDONED
+    }
+    private String id;
+    private String eventId;
+    private long custId;
+    private Status status = Status.PENDING;
+    private Set<Seat> seats = new HashSet<>();
+    private Date created;
+    
+    // TODO: Consider expiry of the shopping cart.
+}
